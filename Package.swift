3cb8f374fa838135521c5617b137a972c237cc3e
Marc Prud'hommeaux <marc@skip.tools>
2025-02-25 15:51:00 -0500

    Update copyright and license headers

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
-        .library(name: "SkipKeychain", targets: ["SkipKeychain"]),
+        .library(name: "SkipKeychain", type: .dynamic, targets: ["SkipKeychain"]),
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "1.2.1"),
