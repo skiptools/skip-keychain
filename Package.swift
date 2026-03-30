5d9ba280aef8824528cfba4ccebd176a452682c4
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-03-29 20:03:22 -0400

    Update headers

--- Package.swift
+++ Package.swift
@@ -1,9 +1,4 @@
 // swift-tools-version: 5.9
-// This is a Skip (https://skip.dev) package,
-// containing a Swift Package Manager project
-// that will use the Skip build plugin to transpile the
-// Swift Package, Sources, and Tests into an
-// Android Gradle Project with Kotlin sources and JUnit tests.
 import PackageDescription
 
 let package = Package(
--- Tests/SkipKeychainTests/SkipKeychainTests.swift
+++ Tests/SkipKeychainTests/SkipKeychainTests.swift
@@ -1,3 +1,5 @@
+// Copyright 2024–2026 Skip
+// SPDX-License-Identifier: MPL-2.0
 import XCTest
 import OSLog
 import Foundation
